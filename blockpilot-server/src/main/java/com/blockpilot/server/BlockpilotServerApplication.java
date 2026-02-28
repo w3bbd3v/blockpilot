831ea2c34e7f82ba70bcf09079a24a8aad3e270b
aw3bbd3v <webb.c.andrew@gmail.com>
2026-02-28 12:35:06 -0500

    Rename server module to be cleaner.

--- blockpilot-server/src/main/java/com/blockpilot/server/BlockpilotServerApplication.java
+++ blockpilot-server/src/main/java/com/blockpilot/server/BlockpilotServerApplication.java
@@ -1,4 +1,4 @@
-package com.blockpilot.blockpilot_server;
+package com.blockpilot.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blockpilot-server/src/test/java/com/blockpilot/server/BlockpilotServerApplicationTests.java
+++ blockpilot-server/src/test/java/com/blockpilot/server/BlockpilotServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.blockpilot.blockpilot_server;
+package com.blockpilot.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
