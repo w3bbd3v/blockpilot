52a162e2828d30126031dd993007ea981c311c6c
aw3bbd3v <webb.c.andrew@gmail.com>
2026-02-28 12:23:50 -0500

    Stub out first blockpilot module.

---
+++ blockpilot-server/src/main/java/com/blockpilot/blockpilot_server/BlockpilotServerApplication.java
@@ -0,0 +1,13 @@
+package com.blockpilot.blockpilot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockpilotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockpilotServerApplication.class, args);
+	}
+
+}
---
+++ blockpilot-server/src/test/java/com/blockpilot/blockpilot_server/BlockpilotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockpilot.blockpilot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockpilotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
